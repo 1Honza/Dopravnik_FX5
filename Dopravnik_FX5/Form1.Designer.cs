6e63adb155f00f1dc2943195557e3bb5b5b8f927
admin <admin@MEK-3NRDVN7>
2024-10-16 11:19:40 +0200

    Přidejte soubory projektu.

---
+++ Dopravnik_FX5/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace Dopravnik_FX5
+{
+    partial class Dopravnik
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.buttonDisconnect = new System.Windows.Forms.Button();
+            this.labelTCP = new System.Windows.Forms.Label();
+            this.textBoxTCP = new System.Windows.Forms.TextBox();
+            this.labelPort = new System.Windows.Forms.Label();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox_S_1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox_S_2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox_S_3 = new System.Windows.Forms.PictureBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_S_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_S_2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_S_3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonConnect.Location = new System.Drawing.Point(212, 16);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(100, 41);
+            this.buttonConnect.TabIndex = 0;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // buttonDisconnect
+            // 
+            this.buttonDisconnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonDisconnect.Location = new System.Drawing.Point(212, 63);
+            this.buttonDisconnect.Name = "buttonDisconnect";
+            this.buttonDisconnect.Size = new System.Drawing.Size(100, 41);
+            this.buttonDisconnect.TabIndex = 1;
+            this.buttonDisconnect.Text = "Disconnect";
+            this.buttonDisconnect.UseVisualStyleBackColor = true;
+            this.buttonDisconnect.Click += new System.EventHandler(this.buttonDisconnect_Click);
+            // 
+            // labelTCP
+            // 
+            this.labelTCP.AutoSize = true;
+            this.labelTCP.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelTCP.Location = new System.Drawing.Point(19, 18);
+            this.labelTCP.Name = "labelTCP";
+            this.labelTCP.Size = new System.Drawing.Size(58, 20);
+            this.labelTCP.TabIndex = 2;
+            this.labelTCP.Text = "TCP/IP";
+            // 
+            // textBoxTCP
+            // 
+            this.textBoxTCP.Location = new System.Drawing.Point(83, 18);
+            this.textBoxTCP.Name = "textBoxTCP";
+            this.textBoxTCP.Size = new System.Drawing.Size(100, 23);
+            this.textBoxTCP.TabIndex = 3;
+            this.textBoxTCP.Text = "192.168.0.10";
+            // 
+            // labelPort
+            // 
+            this.labelPort.AutoSize = true;
+            this.labelPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelPort.Location = new System.Drawing.Point(39, 63);
+            this.labelPort.Name = "labelPort";
+            this.labelPort.Size = new System.Drawing.Size(38, 20);
+            this.labelPort.TabIndex = 4;
+            this.labelPort.Text = "Port";
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(83, 60);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(42, 23);
+            this.textBoxPort.TabIndex = 5;
+            this.textBoxPort.Text = "502";
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelStatus.ForeColor = System.Drawing.Color.Red;
+            this.labelStatus.Location = new System.Drawing.Point(345, 26);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(89, 20);
+            this.labelStatus.TabIndex = 6;
+            this.labelStatus.Text = "Disconnect";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(0, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(505, 10);
+            this.label2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label3.Location = new System.Drawing.Point(31, 203);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(437, 29);
+            this.label3.TabIndex = 8;
+            // 
+            // pictureBox_S_1
+            // 
+            this.pictureBox_S_1.BackColor = System.Drawing.Color.Red;
+            this.pictureBox_S_1.Location = new System.Drawing.Point(83, 152);
+            this.pictureBox_S_1.Name = "pictureBox_S_1";
+            this.pictureBox_S_1.Size = new System.Drawing.Size(34, 28);
+            this.pictureBox_S_1.TabIndex = 9;
+            this.pictureBox_S_1.TabStop = false;
+            // 
+            // pictureBox_S_2
+            // 
+            this.pictureBox_S_2.BackColor = System.Drawing.Color.Red;
+            this.pictureBox_S_2.Location = new System.Drawing.Point(227, 152);
+            this.pictureBox_S_2.Name = "pictureBox_S_2";
+            this.pictureBox_S_2.Size = new System.Drawing.Size(34, 28);
+            this.pictureBox_S_2.TabIndex = 10;
+            this.pictureBox_S_2.TabStop = false;
+            // 
+            // pictureBox_S_3
+            // 
+            this.pictureBox_S_3.BackColor = System.Drawing.Color.Red;
+            this.pictureBox_S_3.Location = new System.Drawing.Point(386, 152);
+            this.pictureBox_S_3.Name = "pictureBox_S_3";
+            this.pictureBox_S_3.Size = new System.Drawing.Size(34, 28);
+            this.pictureBox_S_3.TabIndex = 11;
+            this.pictureBox_S_3.TabStop = false;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonStart.Location = new System.Drawing.Point(62, 264);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(81, 42);
+            this.buttonStart.TabIndex = 12;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonStop.Location = new System.Drawing.Point(203, 264);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(81, 42);
+            this.buttonStop.TabIndex = 13;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonExit.Location = new System.Drawing.Point(412, 264);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(81, 42);
+            this.buttonExit.TabIndex = 14;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // Dopravnik
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(505, 318);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.pictureBox_S_3);
+            this.Controls.Add(this.pictureBox_S_2);
+            this.Controls.Add(this.pictureBox_S_1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.textBoxPort);
+            this.Controls.Add(this.labelPort);
+            this.Controls.Add(this.textBoxTCP);
+            this.Controls.Add(this.labelTCP);
+            this.Controls.Add(this.buttonDisconnect);
+            this.Controls.Add(this.buttonConnect);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Dopravnik";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dopravník";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_S_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_S_2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_S_3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Button buttonDisconnect;
+        private System.Windows.Forms.Label labelTCP;
+        private System.Windows.Forms.TextBox textBoxTCP;
+        private System.Windows.Forms.Label labelPort;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox_S_1;
+        private System.Windows.Forms.PictureBox pictureBox_S_2;
+        private System.Windows.Forms.PictureBox pictureBox_S_3;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonExit;
+    }
+}
+
---
+++ Dopravnik_FX5/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using EasyModbus;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+
+
+namespace Dopravnik_FX5
+{
+    public partial class Dopravnik : Form
+    {
+        bool isconnect = false;
+
+        ModbusClient modbus = new ModbusClient();
+        public Dopravnik()
+        {
+            InitializeComponent();
+        }
+
+        // definovani vlakna
+        Thread thread;
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            // vytvoreni pripojeni
+            modbus.IPAddress = Convert.ToString(textBoxTCP.Text);
+            modbus.Port = Convert.ToInt32(textBoxPort.Text);
+            try
+            {
+                modbus.Connect();
+                isconnect = true;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            if (modbus.Connected == true)
+            {
+                labelStatus.Text = "Connected";
+                labelStatus.ForeColor = Color.Green;
+            }
+        }
+
+        private void buttonDisconnect_Click(object sender, EventArgs e)
+        {
+            // odpojeni, vypnuti vlakna, vypnuti vystupu
+
+            if (isconnect == true)
+            {
+                modbus.Disconnect();
+                labelStatus.Text = "Disconnected";
+                labelStatus.ForeColor = Color.Red;
+
+                if (modbus.Connected == true)
+                {
+                    thread.Abort();
+                }
+                modbus.WriteSingleCoil(2, false); // 0 - 2 - dopravník vpravo
+                modbus.WriteSingleCoil(3, false); // 1 - 3 - dopravník vlevo
+                modbus.WriteSingleCoil(6, false); // 4 - 6 - kontrolka S_1
+                modbus.WriteSingleCoil(7, false); // 5 - 7 - kontrolka S_2
+                modbus.WriteSingleCoil(8, false); // 6 - 8 - kontrolka S_3
+            
+                isconnect = false;
+            }
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (isconnect == true)
+            {
+                // OUTPUT Y2 = 2 - dopravnik vpravo, Y3 = 3 - dopravnik vlevo
+                modbus.WriteSingleCoil(3, false);
+
+                modbus.WriteSingleCoil(2, true);
+
+                // Online read Input
+                thread = new Thread(new ThreadStart(WorkThreadFunction));
+                thread.Start();
+
+                // Read INPUT X4 - S_1, X5 - S_2, X6 - S_3
+                // OUT Y6 - 1. kontrolka S_1, Y7 - 2. kontrolka S_2, Y10 - 3. kontrolka S_3
+                // definovani vlakna pro cteni aktualniho stavu vstupu
+
+                void WorkThreadFunction()
+                {
+                    while (isconnect == true)
+                    {
+                        // read input senzor - zmena barvy 
+                        Color color_S_1;
+                        Color color_S_2;
+                        Color color_S_3;
+
+                        bool[] S_1 = modbus.ReadDiscreteInputs(4, 1); // X4 - S_1 senzor
+                        bool[] S_2 = modbus.ReadDiscreteInputs(5, 1); // X5 - S_2 senzor
+                        bool[] S_3 = modbus.ReadDiscreteInputs(6, 1); // X6 - S_3 senzor
+
+                        // Senzor S_1
+                        if (S_1[0] == true)
+                        {
+                            color_S_1 = Color.Green;
+                            modbus.WriteSingleCoil(3, false); // dopravnik vlevo
+                            Thread.Sleep(100);
+                            modbus.WriteSingleCoil(2, true);  // dopravnik vpravo
+                            modbus.WriteSingleCoil(6, true);  // kontrolka S_1
+                        }
+
+                        else
+                        {
+                            color_S_1 = Color.Red;
+                            modbus.WriteSingleCoil(6, false);  // kontrolka S_1
+                        }
+
+                        // Senzor S_2
+                        if (S_2[0] == true)
+                        {
+                            color_S_2 = Color.Green;
+                            modbus.WriteSingleCoil(7, true);  // kontrolka S_2
+                        }
+
+                        else
+                        {
+                            color_S_2 = Color.Red;
+                            modbus.WriteSingleCoil(7, false); // kontrolka S_2
+                        }
+
+                        // Senzor S_3
+                        if (S_3[0] == true)
+                        {
+                            color_S_3 = Color.Green;
+                            modbus.WriteSingleCoil(2, false); // dopravnik vpravo
+                            Thread.Sleep(100);
+                            modbus.WriteSingleCoil(3, true);  // dopravnik vlevo
+                            modbus.WriteSingleCoil(8, true);  // kontrolka S_3
+                        }
+
+                        else
+                        {
+                            color_S_3 = Color.Red;
+                            modbus.WriteSingleCoil(8, false);  // kontrolka S_3
+                        }
+
+                        // potlaceni vlakna pro prevod do jineho vlakna
+                        {
+                            this.Invoke((MethodInvoker)delegate { pictureBox_S_1.BackColor = color_S_1; });
+                            this.Invoke((MethodInvoker)delegate { pictureBox_S_2.BackColor = color_S_2; });
+                            this.Invoke((MethodInvoker)delegate { pictureBox_S_3.BackColor = color_S_3; });
+                        }
+                    }
+                }
+            }
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            // stop tl. shodi vystupy a vlakno
+
+            if (isconnect == true)
+            {
+                // End online read Input
+                thread.Abort();
+
+                modbus.WriteSingleCoil(2, false);  // 2 - dopravník vpravo
+                modbus.WriteSingleCoil(3, false);  // 3 - dopravník vlevo
+                modbus.WriteSingleCoil(6, false);  // 6 - kontrolka S_1
+                modbus.WriteSingleCoil(7, false);  // 7 - kontrolka S_2
+                modbus.WriteSingleCoil(8, false);  // 8 - kontrolka S_3
+            }
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {    
+            // exit shodi vystupy, vlakno, odpoji se a zavre okno 
+
+            if (isconnect == true)
+            {
+                modbus.WriteSingleCoil(2, false);  // 2 - dopravník vpravo
+                modbus.WriteSingleCoil(3, false);  // 3 - dopravník vlevo
+                modbus.WriteSingleCoil(6, false);  // 6 - kontrolka S_1
+                modbus.WriteSingleCoil(7, false);  // 7 - kontrolka S_2
+                modbus.WriteSingleCoil(8, false);  // 8 - kontrolka S_3
+                modbus.Disconnect();
+            }
+
+            isconnect = false;
+
+            Close();
+        }
+    }
+}
